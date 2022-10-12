93eb88af89d04a4837ff2a68853acafc3bbd4691
Rodrigo Rodirguez <68085175+Roco-001@users.noreply.github.com>
2022-10-12 13:44:07 -0500

    Delete matriz/venv5 directory

--- matriz/venv5/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\inetum\desktop\proyecto oncall\matrices\matriz\venv5\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
